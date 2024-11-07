cc09093e208d28705f90b892f79ba59555a2227a
jcampeau25 <jdccanuck25@gmail.com>
2024-11-07 08:35:05 -0500

    Add project files.

---
+++ Game1.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace Mongame_1___Assignment
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+
+        Texture2D backgroundTexture, kanyeTexture, kendrickTexture;
+        
+
+        List<string> backgrounds = new List<string> { "sunset", "forest", "nightsky"};
+
+        Random generator = new Random();
+
+        
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            _graphics.PreferredBackBufferWidth = 600;
+            _graphics.PreferredBackBufferHeight = 400;
+            _graphics.ApplyChanges();
+
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
+
+            //int background = generator.Next(backgrounds.Count);
+            backgroundTexture = Content.Load<Texture2D>("nightsky");
+            kanyeTexture = Content.Load<Texture2D>("kanye");
+            kendrickTexture = Content.Load<Texture2D>("kendrick");
+
+
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // TODO: Add your drawing code here
+            _spriteBatch.Begin();
+
+            _spriteBatch.Draw(backgroundTexture, new Vector2(0, 0), Color.White);
+
+            _spriteBatch.Draw(kanyeTexture, new Vector2(50,50), Color.White);
+            _spriteBatch.Draw(kendrickTexture, new Vector2(350,350), Color.White);
+
+            _spriteBatch.End();
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,3 @@
+﻿
+using var game = new Mongame_1___Assignment.Game1();
+game.Run();
